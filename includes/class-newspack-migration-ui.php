52cc7a63fd8d3c2fea2f6e344171418b968a7e32
claudiulodro <claudiulodro@gmail.com>
2024-09-25 05:03:58 -0700

    add temp method for getting data

--- includes/class-newspack-migration-ui.php
+++ includes/class-newspack-migration-ui.php
@@ -20,5 +20,17 @@ final class Newspack_Migration_UI {
 	public static function init() {
 
 	}
+
+	/**
+	 * Temporary way to get all of the migrated data.
+	 * We'll want to use whatever general purpose method the migrator provides once ready.
+	 *
+	 * @return array Array of JSON.
+	 */
+	public static function get_migrated_data() {
+		global $wpdb;
+
+		return $wpdb->get_results( "SELECT option_name, option_value FROM $wpdb->options WHERE option_name LIKE 'full_initial_migration1_%'", ARRAY_A );
+	}
 }
 Newspack_Migration_UI::init();
